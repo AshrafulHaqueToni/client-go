81541cf6fdfc6a55b16ef3c0d6e857767ec17295
ashraful <ashraf@appscode.com>
2023-06-12 18:25:13 +0600

    update in cluster

--- In-cluster-client-configuaration/main.go
+++ In-cluster-client-configuaration/main.go
@@ -9,10 +9,10 @@ import (
 )
 
 func main() {
-
 	config, err := rest.InClusterConfig()
+	fmt.Println(config)
 	if err != nil {
-		fmt.Printf("error %s getting inclusterconfig\n", err.Error())
+		fmt.Printf("error %s getting inclusterconfig", err.Error())
 	}
 
 	// create a Kubernetes clientset using the kubernetes.NewForConfig() function and use it to interact with the cluster.
@@ -21,21 +21,21 @@ func main() {
 		fmt.Printf("error %s, creating clientset\n", err.Error())
 	}
 
-	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		fmt.Printf("error %s, while listing all the pods from default namespace\n", err.Error())
-	}
-	fmt.Println("Pods from default namespace")
-
-	for _, pod := range pods.Items {
-		fmt.Printf("%s ", pod.Name)
-	}
-	_, err = clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		fmt.Printf("listing deployments %s\n", err.Error())
-	}
-	fmt.Println("\nDeployment from default namespace")
-
 	for {
+		pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			fmt.Printf("error %s, while listing all the pods from default namespace\n", err.Error())
+		}
+		fmt.Println("Pods from default namespace")
+
+		for _, pod := range pods.Items {
+			fmt.Printf("%s ", pod.Name)
+		}
+		_, err = clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			fmt.Printf("listing deployments %s\n", err.Error())
+		}
+		fmt.Println("\nDeployment from default namespace")
+
 	}
 }
